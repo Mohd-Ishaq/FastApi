8a1c3004600d7b6479d52f52e8ec6d07eeb7c210
Mohd-Ishaq <ishaqmohd116@gmail.com>
2022-04-21 02:03:04 +0530

    added cors'

--- app/main.py
+++ app/main.py
@@ -4,11 +4,21 @@ from .database import engine, get_db
 from .router import user
 from .router import post, auth
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = ["*"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(post.router)
